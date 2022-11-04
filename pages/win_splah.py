ff14a4b0957f2906891bc8caf459bf4c95878e9f
Heymen22 <49359702+Heymen22@users.noreply.github.com>
2022-11-04 18:46:57 +0300

    Перевод win_splash.py

--- pages/win_splah.py
+++ pages/win_splah.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-"""启动屏幕"""
-# 需要实现隐藏边框和标题
+"""Заставка"""
+# Необходимо реализовать скрытую рамку и заголовок
 
 import os
 from tkinter import Canvas, Label, Tk
@@ -13,11 +13,11 @@ from lib.functions import set_window_center
 
 
 class Splah(Tk):
-    """初始化启动屏幕窗口"""
+    """Инициализировать окно заставки"""
 
     def __init__(self):
         Tk.__init__(self)
-        self.title("程序加载中")
+        self.title("Загрузка программы")
         self.w = 300
         self.h = 300
         set_window_center(self, self.w, self.h)
@@ -25,7 +25,7 @@ class Splah(Tk):
         self.splash()
 
     def splash(self):
-        """启动屏幕"""
+        """Заставка"""
         image_file = os.path.join(
             glv.get_variable("APP_PATH"),
             glv.get_variable("DATA_DIR"),
@@ -34,8 +34,8 @@ class Splah(Tk):
         )
         canvas = Canvas(self, width=self.w, height=250, bg="white")
         # if os.path.exists(image_file):
-        #     img = Image.open(image_file)  # 打开图片
-        #     image = ImageTk.PhotoImage(img)  # 用PIL模块的PhotoImage打开
+        #     img = Image.open(image_file)  # Откройте картинку
+        #     image = ImageTk.PhotoImage(img)  # Открыть с помощью PhotoImage из модуля PIL
         #     canvas.create_image(self.w / 2, 250 / 2, image=image)
         # else:
         canvas.create_text(
@@ -43,10 +43,10 @@ class Splah(Tk):
         )
 
         canvas.pack(fill="both")
-        Label(self, text="欢迎使用", bg="green", fg="#fff", height=2).pack(
+        Label(self, text="Добро пожаловать", bg="green", fg="#fff", height=2).pack(
             fill="both", side="bottom"
         )
 
-        # 设置splash显示的时间，单位是毫秒（milliseconds）
+        # Установите время отображения заставки, единица измерения — миллисекунды (миллисекунды)
         self.after(4000, self.destroy)
         self.mainloop()
