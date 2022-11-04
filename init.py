c538972a27a54f7fbf946d933ce437caadbdf88c
Heymen22 <49359702+Heymen22@users.noreply.github.com>
2022-11-04 18:29:05 +0300

    Update init.py
    Переведено то что можно было перевести. Что нельзя было перевести, тоже переведно, но сохранен оригинал.

--- init.py
+++ init.py
@@ -10,38 +10,38 @@ from main import App
 
 
 class InitWindow(Tk):
-    """初始化窗口"""
+    """Инициализировать окно или Окно инициализации"""
 
     def __init__(self):
         Tk.__init__(self)
-        self.title("初始化数据")
+        self.title("Данные инициализации")
         set_window_center(self, 300, 180)
         self.resizable(False, False)
-        self.win_success = None # 初始化成功的提示窗口
+        self.win_success = None # Окно сообщения об успешной инициализации
         self.init_page()
 
     def init_page(self):
-        """加载控件"""
-        btn_1 = Button(self, text="初始化数据库", command=self.do_init_db)
+        """Контроль нагрузки ??? (ориг. 加载控件)"""
+        btn_1 = Button(self, text="Инициализировать базу данных", command=self.do_init_db)
         btn_1.pack(expand="yes", padx=10, pady=10, ipadx=5, ipady=5)
 
     def do_init_db(self):
-        """初始化"""
+        """Инициализация"""
         db_helper = DBHelper()
         db_helper.reset_database()
         db_helper.create_database()
         try:
-            tmp = db_helper.insert_user("admin", "admin")  # 默认用户
+            tmp = db_helper.insert_user("admin", "admin")  # Пользователь по умолчанию
             tmp2 = db_helper.insert_content_by_username(
                 "admin",
                 "Hello World !",
-                "源码仓库地址：https://github.com/doudoudzj/tkinter-app",
+                "Адрес склада исходного кода ??? (ориг. 源码仓库地址)：https://github.com/doudoudzj/tkinter-app",
                 "github",
             )
             tmp3 = db_helper.get_content_by_username("admin")
-            print("添加用户admin:", tmp)
-            print("添加内容:", tmp2)
-            print("查询内容:", tmp3)
+            print("Добавить пользователя admin:", tmp)
+            print("Добавить контент ??? (ориг. 添加内容):", tmp2)
+            print("Содержание запроса ??? (ориг. 查询内容):", tmp3)
             self.do_success()
             self.destroy()
         except KeyError:
@@ -49,29 +49,29 @@ class InitWindow(Tk):
             self.do_failed()
 
     def do_failed(self):
-        """是否重试"""
-        res = tkinter.messagebox.askretrycancel('提示', '初始化失败，是否重试？', parent=self)
+        """Стоит ли повторить ??? (ориг. 是否重试)"""
+        res = tkinter.messagebox.askretrycancel('Намекать ??? (ориг. 提示)', 'Ошибка инициализации, повторить попытку?', parent=self)
         if res is True:
             self.do_init_db()
         elif res is False:
             self.destroy()
 
     def do_success(self):
-        """初始化成功弹窗"""
+        """Всплывающее окно об успешной инициализации"""
         self.win_success = Tk()
-        self.win_success.title("初始化成功")
+        self.win_success.title("Инициализация прошла успешно")
         set_window_center(self.win_success, 250, 150)
         self.win_success.resizable(False, False)
-        msg = Label(self.win_success, text="初始化成功")
+        msg = Label(self.win_success, text="Инициализация прошла успешно")
         msg.pack(expand="yes", fill="both")
 
-        btn = Button(self.win_success, text="确定", command=self.quit)
+        btn = Button(self.win_success, text="Конечно ??? (ориг. 确定)", command=self.quit)
         btn.pack(side="right", padx=10, pady=10, ipadx=5, ipady=5)
-        btn_open_app = Button(self.win_success, text="启动程序", command=self.open_app)
+        btn_open_app = Button(self.win_success, text="Стартовая программа ??? (ориг. 启动程序)", command=self.open_app)
         btn_open_app.pack(side="right", padx=10, pady=10, ipadx=5, ipady=5)
 
     def open_app(self):
-        """打开应用程序"""
+        """Откройте приложение ??? (ориг. 打开应用程序)"""
         self.quit()
         self.win_success.destroy()
         self.win_success.quit()
